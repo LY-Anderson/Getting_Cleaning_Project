b96ec5e4e5eb4465b832e5e89043ad757732bc0e
LY-Anderson <lyanderson@iiroc.ca>
2015-02-18 20:24:50 -0500

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,26 @@
+# Code for Getting_Cleaning_Project
+# Course project for Coursera Course
+# Getting and Cleaning Data
+
+# Get training and test data sets from
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+# Merge training and test data sets
+
+# Extract the mean and standard deviation for each measurement
+
+# Use "descriptive activity names" to name the activities in the data set
+
+# Label the data set with descriptive variable names (same as above?)
+
+# Create a second, independent, tidy data set with the average of each variable
+# for each activity and each subject
+
+# Upload a data set as a txt file created with write.table() using
+# row.name=FALSE
+
+
+
+
+
+
